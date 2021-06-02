5b89b9285457a31ac91a2d22e5f356b49d3cd7d8
DevBerringer <blakeberringer@gmail.com>
2021-06-01 19:11:04 -0500

    updated header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <span onClick={() => window.scroll(0, 0)} className="header bg-dark">
-      🎬 Entertainment Hub 📺
+      🎬 Entertainment Browser 📺
     </span>
   );
 };
