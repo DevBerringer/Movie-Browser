a3c302308dd9bf5c2d3ae2b479f7a106e73d5afc
DevBerringer <blakeberringer@gmail.com>
2021-06-01 22:00:12 -0500

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
 import "./Header.css";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router";
+
 
 const Header = () => {
+
+  const history = useHistory();
+
+  const titleBtn = () => {
+    history.push('/')
+  }
+
+  const searchBtn = () => {
+    history.push('/search')
+  }
+
   return (
-    <span onClick={() => window.scroll(0, 0)} className="header bg-dark">
-      🎬 Entertainment Browser 📺
-    </span>
+    <div className="header bg-dark"
+          onClick={() => window.scroll(0, 0)}>
+      <span onClick={titleBtn} className="header_title">
+        🎬 Entertainment Browser 📺
+      </span>
+      <Button style={{color: "white"}}
+              onClick={searchBtn}
+      >
+        Find Movies or Shows
+      </Button>
+    </div>
   );
 };
 
